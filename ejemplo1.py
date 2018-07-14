36ad5913dd11253a811828ef862d9e394e0c608b
Rodrigo <rjcholca@gmail.com>
2018-07-13 20:48:53 -0500

    mi primer deber en git

---
+++ ejemplo1.py
@@ -0,0 +1,8 @@
+### Introducir un nuumero por teclado y decir si es par o impar
+
+num = input('Introduzca un numero: ')
+if num%2 == 0:
+    print 'Este numero es par'
+else:
+    print 'Este numero es impar'
+ 
